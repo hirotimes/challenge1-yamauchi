8aa4fe3a007155fa9b18668aa79656ea6b30cc62
hirotimes <hrtk.y061@gmail.com>
2024-07-03 20:30:24 +0900

    chore: 画像ファイル追加

--- lib/constants/asset_path.dart
+++ lib/constants/asset_path.dart
@@ -4,4 +4,5 @@ const String spotsJson = 'assets/spots.json';
 /// サムネイルがない場合に表示する画像
 const String imagePathCar = 'assets/images/car.png';
 
+/// マーカー画像
 const String imagePathMarker = 'assets/images/marker.png';
